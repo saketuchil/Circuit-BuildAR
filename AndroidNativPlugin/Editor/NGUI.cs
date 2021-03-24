948c136b762159b4f4a9ad6f1c15fbc2b326b8a9
Saket Uchil <43895539+saketuchil@users.noreply.github.com>
2021-03-24 11:39:04 +0530

    Add files via upload

---
+++ AndroidNativPlugin/Editor/NGUI.cs
@@ -0,0 +1,34 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class NGUI
+{
+    
+
+    public static class uGUITools
+    {
+        [MenuItem("Tools/Anchors to Corners %#A")]
+        static void AnchorsToCorners()
+        {
+            foreach (Transform transform in Selection.transforms)
+            {
+                RectTransform t = transform as RectTransform;
+                RectTransform pt = Selection.activeTransform.parent as RectTransform;
+
+                if (t == null || pt == null) return;
+
+                Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+                                                    t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+                Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+                                                    t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+                t.anchorMin = newAnchorsMin;
+                t.anchorMax = newAnchorsMax;
+                t.offsetMin = t.offsetMax = new Vector2(0, 0);
+            }
+        }
+    }
+}
---
+++ AndroidNativPlugin/Scripts/AndroidPlayerSettings/ChangeSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChangeSettings : MonoBehaviour {
+
+
+	void Awake()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.PlayerSettings.Android.forceSDCardPermission = true;
+		#endif
+	}
+
+}
